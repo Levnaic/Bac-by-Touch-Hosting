418c556b26c5fe8b50e56fa1a9b2e6497c08d388
Levnaic <srdjanlevnaic@gmail.com>
2024-04-24 18:31:06 +0200

    pokuasj analitike

--- app/views/partials/head.php
+++ app/views/partials/head.php
@@ -2,24 +2,18 @@
 <html lang="en">
 
 <head>
-    <!-- Google Tag Manager -->
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-C0MH62YEK1"></script>
     <script>
-        (function(w, d, s, l, i) {
-            w[l] = w[l] || [];
-            w[l].push({
-                'gtm.start': new Date().getTime(),
-                event: 'gtm.js'
-            });
-            var f = d.getElementsByTagName(s)[0],
-                j = d.createElement(s),
-                dl = l != 'dataLayer' ? '&l=' + l : '';
-            j.async = true;
-            j.src =
-                'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
-            f.parentNode.insertBefore(j, f);
-        })(window, document, 'script', 'dataLayer', 'GTM-P8ZK5X2W');
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+
+        gtag('config', 'G-C0MH62YEK1');
     </script>
-    <!-- End Google Tag Manager -->
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta name="description" content="Discover Bač By Touch, your guide to exploring historical monuments and tourism in Bač, Serbia. Plan your trip with us today.">
@@ -27,10 +21,6 @@
     <link rel="icon" type="image/x-icon" href="/assets/img/favicon/favicon.png" />
     <link rel="stylesheet" href="/assets/styles/font-awesome-4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="/assets/styles/css/<?php echo $css; ?>" />
-
 </head>
 
 <body>
-    <!-- Google Tag Manager (noscript) -->
-    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-P8ZK5X2W" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-    <!-- End Google Tag Manager (noscript) -->
